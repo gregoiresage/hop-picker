7ecca824276aaa68bdeb6e2677b4927dafdcb226
Grégoire Sage <gregoire.sage@gmail.com>
2014-03-04 00:03:14 +0100

    add date display and hour hand option

--- src/main.c
+++ src/main.c
@@ -6,33 +6,56 @@ static Layer  *line_layer;
 static InverterLayer* inverter_layer;
 
 GFont custom_font;
+GFont small_font;
 
 static GPath *hour_arrow[2];
-static const GPathInfo HOUR_HAND_POINTS[2] = {
+static const GPathInfo LINE_HAND_POINTS[2] = {
 	{
-  	4,
-    (GPoint []) {
-      {-3, 0},
-      {-3, -300},
-	  {3, -300},
-	  {3, 0},
-    }
+		4,
+		(GPoint []) {
+			{-3, 0},
+			{-3, -300},
+			{3, -300},
+			{3, 0},
+		}
 	},
 	{
-  	4,
-    (GPoint []) {
-      {-3, 0},
-	  {3, 0},
-	  {3, -300},
-      {-3, -300}, 
-    }
+		4,
+		(GPoint []) {
+			{-3, 0},
+			{3, 0},
+			{3, -300},
+			{-3, -300}, 
+		}
+	}
+};
+static const GPathInfo ARROW_HAND_POINTS[2] = {
+	{
+		4,
+		(GPoint []) {
+			{-6, 0},
+			{-2, -175},
+			{2, -175},
+			{6, 0},
+		}
+	},
+	{
+		4,
+		(GPoint []) {
+			{-6, 0},
+			{6, 0},
+			{2, -175},
+			{-2, -175},
+		}
 	}
 };
 
 static char* txt[] = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
 
+static char date_text[3] = "31";
+
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
- layer_mark_dirty(window_get_root_layer(window));
+	layer_mark_dirty(window_get_root_layer(window));
 }
 
 static void drawClock(GPoint center, int hour, GContext *ctx){
@@ -43,8 +66,9 @@ static void drawClock(GPoint center, int hour, GContext *ctx){
 	const int16_t secondHandLengthC = 180;
 
 	graphics_context_set_fill_color(ctx, getColor() ? GColorBlack : GColorWhite);
+
 	graphics_fill_circle(ctx, center, 180);
-	
+		
 	graphics_context_set_text_color(ctx, getColor() ? GColorWhite : GColorBlack );
 	
 	int minhour=(hour+24)-2;
@@ -52,10 +76,10 @@ static void drawClock(GPoint center, int hour, GContext *ctx){
 	
 	for(int i=minhour*4; i<maxhour*4; i++){
 		int32_t angle = TRIG_MAX_ANGLE * (i % (12*4)) / (12*4);
- 		segA.y = (int16_t)(-cos_lookup(angle) * (int32_t)secondHandLengthA / TRIG_MAX_RATIO) + center.y;
- 		segA.x = (int16_t)(sin_lookup(angle) * (int32_t)secondHandLengthA / TRIG_MAX_RATIO) + center.x;
+		segA.y = (int16_t)(-cos_lookup(angle) * (int32_t)secondHandLengthA / TRIG_MAX_RATIO) + center.y;
+		segA.x = (int16_t)(sin_lookup(angle) * (int32_t)secondHandLengthA / TRIG_MAX_RATIO) + center.x;
 		segC.y = (int16_t)(-cos_lookup(angle) * (int32_t)secondHandLengthC / TRIG_MAX_RATIO) + center.y;
- 		segC.x = (int16_t)(sin_lookup(angle) * (int32_t)secondHandLengthC / TRIG_MAX_RATIO) + center.x;
+		segC.x = (int16_t)(sin_lookup(angle) * (int32_t)secondHandLengthC / TRIG_MAX_RATIO) + center.x;
 		
 		if(i % 4 == 0) {
 			//graphics_draw_line(ctx, segA, segC);
@@ -65,22 +89,22 @@ static void drawClock(GPoint center, int hour, GContext *ctx){
 			graphics_fill_circle(	ctx, segC, 6);
 			
 			if(clock_is_24h_style()){
-			graphics_draw_text(ctx,
-      			txt[(i % (24*4))/4],
-      			custom_font,
-      			GRect(segA.x-25, segA.y-25, 50, 50),
-		  		GTextOverflowModeWordWrap,
-      			GTextAlignmentCenter,
-      			NULL);	
+				graphics_draw_text(ctx,
+					txt[(i % (24*4))/4],
+					custom_font,
+					GRect(segA.x-25, segA.y-25, 50, 50),
+					GTextOverflowModeWordWrap,
+					GTextAlignmentCenter,
+					NULL);	
 			}
 			else {
-			graphics_draw_text(ctx,
-      			(i % (12*4))/4 == 0 ? "12" : txt[(i % (12*4))/4],
-      			custom_font,
-      			GRect(segA.x-25, segA.y-25, 50, 50),
-		  		GTextOverflowModeWordWrap,
-      			GTextAlignmentCenter,
-      			NULL);
+				graphics_draw_text(ctx,
+					(i % (12*4))/4 == 0 ? "12" : txt[(i % (12*4))/4],
+					custom_font,
+					GRect(segA.x-25, segA.y-25, 50, 50),
+					GTextOverflowModeWordWrap,
+					GTextAlignmentCenter,
+					NULL);
 			}
 		}
 		else {
@@ -88,6 +112,40 @@ static void drawClock(GPoint center, int hour, GContext *ctx){
 			graphics_fill_circle(	ctx, segC, 3);
 		}
 	}
+}
+
+static void drawDate(GPoint center, int angle, int date, GContext *ctx){
+	GPoint segA;
+	// const int16_t datePosFromCenter = 92;
+	const int16_t datePosFromCenter = 80;
+
+	int16_t posFromCenter = 0;
+	if(angle <= TRIG_MAX_ANGLE/4 || (angle >= TRIG_MAX_ANGLE/2 && angle <= 3*TRIG_MAX_ANGLE/4)){
+		posFromCenter = 80 + 12.0 * (angle % (TRIG_MAX_ANGLE/2)) / (TRIG_MAX_ANGLE / 4);
+	}
+	else {
+		posFromCenter = 92 - 12.0 * (angle % (TRIG_MAX_ANGLE/4)) / (TRIG_MAX_ANGLE / 4);
+	}
+
+	segA.y = (int16_t)(-cos_lookup(angle) * (int32_t)posFromCenter / TRIG_MAX_RATIO) + center.y;
+	segA.x = (int16_t)(sin_lookup(angle) * (int32_t)posFromCenter / TRIG_MAX_RATIO) + center.x;
+
+	snprintf(date_text, sizeof(date_text), "%d", date); 
+
+	// graphics_context_set_stroke_color(ctx, getColor() ? GColorWhite : GColorBlack);
+	graphics_context_set_fill_color(ctx, getColor() ? GColorWhite : GColorBlack);
+	graphics_fill_circle(ctx, GPoint(segA.x, segA.y), 14);
+	graphics_context_set_fill_color(ctx, getColor() ? GColorBlack : GColorWhite);
+	graphics_fill_circle(ctx, GPoint(segA.x, segA.y), 12);
+	// graphics_draw_circle(ctx, GPoint(segA.x, segA.y), 12);
+	graphics_context_set_text_color(ctx, getColor() ? GColorWhite : GColorBlack );
+	graphics_draw_text(ctx,
+					date_text,
+					small_font,
+					GRect(segA.x-12, segA.y-11, 25, 25),
+					GTextOverflowModeWordWrap,
+					GTextAlignmentCenter,
+					NULL);
 }
 
 static void linelayer_update_proc(Layer *layer, GContext *ctx) {
@@ -107,58 +165,85 @@ static void linelayer_update_proc(Layer *layer, GContext *ctx) {
 	
 	drawClock(secondHand, t->tm_hour, ctx);
 	
-  	graphics_context_set_stroke_color(ctx, getColor() ? GColorBlack : GColorWhite);
+	graphics_context_set_stroke_color(ctx, getColor() ? GColorBlack : GColorWhite);
 	graphics_context_set_fill_color(ctx, getColor() ? GColorWhite : GColorBlack);
 	
 	for(int i=0; i<2; i++){
 		gpath_move_to(hour_arrow[i], secondHand);
 		gpath_rotate_to(hour_arrow[i], second_angle);
 		gpath_draw_filled(ctx, hour_arrow[i]);
-  		gpath_draw_outline(ctx, hour_arrow[i]);
+		gpath_draw_outline(ctx, hour_arrow[i]);
+	}
+
+	if(getDate()){
+		drawDate(secondHand, second_angle, t->tm_mday, ctx);
 	}
 }
 
 static void window_load(Window *window) {
- Layer *window_layer = window_get_root_layer(window);
- GRect bounds = layer_get_bounds(window_layer);
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
 	
- line_layer = layer_create(bounds);
- layer_set_update_proc(line_layer, linelayer_update_proc);
- layer_add_child(window_layer, line_layer);
+	line_layer = layer_create(bounds);
+	layer_set_update_proc(line_layer, linelayer_update_proc);
+	layer_add_child(window_layer, line_layer);
 	
- inverter_layer = inverter_layer_create	(bounds);
- layer_set_hidden(inverter_layer_get_layer(inverter_layer), !getInverted());
- layer_add_child(window_layer, inverter_layer_get_layer(inverter_layer));
+	inverter_layer = inverter_layer_create	(bounds);
+	layer_set_hidden(inverter_layer_get_layer(inverter_layer), !getInverted());
+	layer_add_child(window_layer, inverter_layer_get_layer(inverter_layer));
 
 }
 
 static void window_unload(Window *window) {
- layer_destroy(line_layer);
- inverter_layer_destroy(inverter_layer);
+	layer_destroy(line_layer);
+	inverter_layer_destroy(inverter_layer);
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
   // call autoconf_in_received_handler
-  autoconfig_in_received_handler(iter, context);
+	autoconfig_in_received_handler(iter, context);
 
-  layer_set_hidden(inverter_layer_get_layer(inverter_layer), !getInverted());
-  layer_mark_dirty(line_layer);
+	for(int i=0; i<2; i++){
+		gpath_destroy(hour_arrow[i]);
+	}
+	if(getHand() == HAND_LINE){
+		for(int i=0; i<2; i++){
+			hour_arrow[i] = gpath_create(&LINE_HAND_POINTS[i]);
+		}
+	}
+	else {
+		for(int i=0; i<2; i++){
+			hour_arrow[i] = gpath_create(&ARROW_HAND_POINTS[i]);
+		}
+	}
+
+	layer_set_hidden(inverter_layer_get_layer(inverter_layer), !getInverted());
+	layer_mark_dirty(line_layer);
 }
 
 static void init(void) {
 	autoconfig_init();
-    window = window_create();
+	window = window_create();
 	
 	window_set_window_handlers(window, (WindowHandlers) {
-   	.load = window_load,
-   	.unload = window_unload,	
-    });
+		.load = window_load,
+		.unload = window_unload,	
+	});
 
 	custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_COMFORTAA_40));
+	small_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_COMFORTAA_18));
 
-	for(int i=0; i<2; i++){
-		hour_arrow[i] = gpath_create(&HOUR_HAND_POINTS[i]);
-	}	
+	if(getHand() == HAND_LINE){
+		for(int i=0; i<2; i++){
+			hour_arrow[i] = gpath_create(&LINE_HAND_POINTS[i]);
+		}
+	}
+	else {
+		for(int i=0; i<2; i++){
+			hour_arrow[i] = gpath_create(&ARROW_HAND_POINTS[i]);
+		}
+	}
+
 
 	app_message_register_inbox_received(in_received_handler);
 
@@ -170,6 +255,7 @@ static void init(void) {
 static void deinit(void) {
 	autoconfig_deinit();
 	fonts_unload_custom_font(custom_font);
+	fonts_unload_custom_font(small_font);
 	for(int i=0; i<2; i++){
 		gpath_destroy(hour_arrow[i]);
 	}
