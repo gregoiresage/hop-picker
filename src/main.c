8783805d5ba3d88b37df31c44df63181e39fb899
Grégoire Sage <gregoire.sage@gmail.com>
2014-03-16 15:12:44 +0100

    add useless animation :-p

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "autoconfig.h"
 
 static Window *window;
-static Layer  *line_layer;
+static Layer  *layer;
 static InverterLayer* inverter_layer;
 
 GFont custom_font;
@@ -10,49 +10,25 @@ GFont small_font;
 
 static GPath *hour_arrow[2];
 static const GPathInfo LINE_HAND_POINTS[2] = {
-	{
-		4,
-		(GPoint []) {
-			{-3, 0},
-			{-3, -300},
-			{3, -300},
-			{3, 0},
-		}
-	},
-	{
-		4,
-		(GPoint []) {
-			{-3, 0},
-			{3, 0},
-			{3, -300},
-			{-3, -300}, 
-		}
-	}
+	{4,(GPoint []) {{-3, 0},{-3, -300},{3, -300},{3, 0}}},
+	{4,(GPoint []) {{-3, 0},{3, 0},{3, -300},{-3, -300}}}
 };
 static const GPathInfo ARROW_HAND_POINTS[2] = {
-	{
-		4,
-		(GPoint []) {
-			{-6, 0},
-			{-2, -175},
-			{2, -175},
-			{6, 0},
-		}
-	},
-	{
-		4,
-		(GPoint []) {
-			{-6, 0},
-			{6, 0},
-			{2, -175},
-			{-2, -175},
-		}
-	}
+	{4,(GPoint []) {{-6, 0},{-2, -175},{2, -175},{6, 0}}},
+	{4,(GPoint []) {{-6, 0},{6, 0},{2, -175},{-2, -175}}}
 };
 
 static char* txt[] = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
 
 static char date_text[3] = "31";
+
+static AppTimer *timer;
+
+static int anim_minutes = 0;
+static bool isAnimating = false;
+static int increaseAnimation = 20;
+
+
 
 static bool containsCircle(GPoint center, int radius){
 	return center.x - radius > 0 && center.x + radius < 144 && center.y - radius > 0 && center.y + radius < 168;
@@ -60,6 +36,13 @@ static bool containsCircle(GPoint center, int radius){
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 	layer_mark_dirty(window_get_root_layer(window));
+}
+
+static void animation_timer_callback(void *data) {
+	timer = NULL;
+	layer_mark_dirty(window_get_root_layer(window));
+  	if(isAnimating)
+  		timer = app_timer_register(70 /* milliseconds */, animation_timer_callback, NULL);
 }
 
 static void drawClock(GPoint center, int hour, GContext *ctx){
@@ -86,7 +69,6 @@ static void drawClock(GPoint center, int hour, GContext *ctx){
 		segC.x = (int16_t)(sin_lookup(angle) * (int32_t)secondHandLengthC / TRIG_MAX_RATIO) + center.x;
 		
 		if(i % 4 == 0) {
-			//graphics_draw_line(ctx, segA, segC);
 			graphics_context_set_fill_color(ctx, getColor() ? GColorBlack : GColorWhite);
 			graphics_fill_circle(	ctx, segC, 8);
 			graphics_context_set_fill_color(ctx, getColor() ? GColorWhite : GColorBlack);
@@ -131,12 +113,10 @@ static void drawDate(GPoint center, int angle, int date, GContext *ctx){
 
 	snprintf(date_text, sizeof(date_text), "%d", date); 
 
-	// graphics_context_set_stroke_color(ctx, getColor() ? GColorWhite : GColorBlack);
 	graphics_context_set_fill_color(ctx, getColor() ? GColorWhite : GColorBlack);
 	graphics_fill_circle(ctx, GPoint(segA.x, segA.y), 14);
 	graphics_context_set_fill_color(ctx, getColor() ? GColorBlack : GColorWhite);
 	graphics_fill_circle(ctx, GPoint(segA.x, segA.y), 12);
-	// graphics_draw_circle(ctx, GPoint(segA.x, segA.y), 12);
 	graphics_context_set_text_color(ctx, getColor() ? GColorWhite : GColorBlack );
 	graphics_draw_text(ctx,
 					date_text,
@@ -147,32 +127,55 @@ static void drawDate(GPoint center, int angle, int date, GContext *ctx){
 					NULL);
 }
 
-static void linelayer_update_proc(Layer *layer, GContext *ctx) {
+static void drawHand(GPoint secondHand, int angle, GContext *ctx){
+	graphics_context_set_stroke_color(ctx, getColor() ? GColorBlack : GColorWhite);
+	graphics_context_set_fill_color(ctx, getColor() ? GColorWhite : GColorBlack);
+	
+	for(int i=0; i<2; i++){
+		gpath_move_to(hour_arrow[i], secondHand);
+		gpath_rotate_to(hour_arrow[i], angle);
+		gpath_draw_filled(ctx, hour_arrow[i]);
+		gpath_draw_outline(ctx, hour_arrow[i]);
+	}
+}
+
+static void layer_update_proc(Layer *layer, GContext *ctx) {
 	GRect bounds = layer_get_bounds(layer);
 	const GPoint center = grect_center_point(&bounds);
 	const int16_t secondHandLength = 150;
 
 	GPoint secondHand;
 
+	int hours = 0;
+	int minutes = 0;
+
 	time_t now = time(NULL);
 	struct tm *t = localtime(&now);
-	
-	int32_t second_angle = TRIG_MAX_ANGLE * ((t->tm_hour % 12) * 60 + t->tm_min) / (12 * 60);
+
+	if(isAnimating){
+		hours = anim_minutes / 60;
+		minutes = anim_minutes - hours * 60;
+		if((increaseAnimation > 0 && anim_minutes >= t->tm_hour * 60 + t->tm_min) || (increaseAnimation < 0 && anim_minutes <= t->tm_hour * 60 + t->tm_min)){
+			hours = t->tm_hour;
+			minutes = t->tm_min;
+			isAnimating = false;
+		}
+		anim_minutes+=increaseAnimation;
+	}
+	else {
+		hours = t->tm_hour;
+		minutes = t->tm_min;
+	}
+
+
+	int32_t second_angle = TRIG_MAX_ANGLE * ((hours % 12) * 60 + minutes) / (12 * 60);
 
 	secondHand.y = (int16_t)(cos_lookup(second_angle) * (int32_t)secondHandLength / TRIG_MAX_RATIO) + center.y;
 	secondHand.x = (int16_t)(-sin_lookup(second_angle) * (int32_t)secondHandLength / TRIG_MAX_RATIO) + center.x;
+
+	drawClock(secondHand, hours, ctx);
 	
-	drawClock(secondHand, t->tm_hour, ctx);
-	
-	graphics_context_set_stroke_color(ctx, getColor() ? GColorBlack : GColorWhite);
-	graphics_context_set_fill_color(ctx, getColor() ? GColorWhite : GColorBlack);
-	
-	for(int i=0; i<2; i++){
-		gpath_move_to(hour_arrow[i], secondHand);
-		gpath_rotate_to(hour_arrow[i], second_angle);
-		gpath_draw_filled(ctx, hour_arrow[i]);
-		gpath_draw_outline(ctx, hour_arrow[i]);
-	}
+	drawHand(secondHand, second_angle, ctx);
 
 	if(getDate()){
 		drawDate(secondHand, second_angle, t->tm_mday, ctx);
@@ -183,9 +186,9 @@ static void window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
 	
-	line_layer = layer_create(bounds);
-	layer_set_update_proc(line_layer, linelayer_update_proc);
-	layer_add_child(window_layer, line_layer);
+	layer = layer_create(bounds);
+	layer_set_update_proc(layer, layer_update_proc);
+	layer_add_child(window_layer, layer);
 	
 	inverter_layer = inverter_layer_create	(bounds);
 	layer_set_hidden(inverter_layer_get_layer(inverter_layer), !getInverted());
@@ -193,8 +196,45 @@ static void window_load(Window *window) {
 
 }
 
+static void window_appear(Window *window){
+	isAnimating = true;
+	time_t now = time(NULL);
+	struct tm *t = localtime(&now);
+
+	int targetMinutes = 0;
+	increaseAnimation = 6;
+	if(clock_is_24h_style()){
+		targetMinutes = t->tm_hour * 60 + t->tm_min;
+		if(targetMinutes < 6*60){
+			anim_minutes = 0;
+		}
+		else if(targetMinutes < 12*60){
+			anim_minutes = 12 * 60;
+			increaseAnimation = -increaseAnimation;
+		}
+		else if(targetMinutes < 18*60){
+			anim_minutes = 12 * 60;
+		}
+		else {
+			anim_minutes = 24 * 60;
+			increaseAnimation = -increaseAnimation;
+		}
+	}
+	else {
+		anim_minutes = 0;
+		targetMinutes = (t->tm_hour % 12) * 60 + t->tm_min;
+		if(targetMinutes > 6*60)
+			increaseAnimation = -increaseAnimation;
+
+	}
+
+
+
+	timer = app_timer_register(100 /* milliseconds */, animation_timer_callback, NULL);
+}
+
 static void window_unload(Window *window) {
-	layer_destroy(line_layer);
+	layer_destroy(layer);
 	inverter_layer_destroy(inverter_layer);
 }
 
@@ -217,8 +257,19 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
 	}
 
 	layer_set_hidden(inverter_layer_get_layer(inverter_layer), !getInverted());
-	layer_mark_dirty(line_layer);
+	layer_mark_dirty(layer);
 }
+
+static void handle_accel_tap(AccelAxisType axis, int32_t direction)
+{
+ 
+   // App Messages can be sent while using Pebble Autoconfig
+	DictionaryIterator *iter;
+	app_message_outbox_begin(&iter);
+	dict_write_cstring(iter, CALENDAR_PKEY, "Knock, knock!");
+	dict_write_end(iter);
+  	app_message_outbox_send();
+}  // handle_accel_tap()
 
 static void init(void) {
 	autoconfig_init();
@@ -227,6 +278,7 @@ static void init(void) {
 	window_set_window_handlers(window, (WindowHandlers) {
 		.load = window_load,
 		.unload = window_unload,	
+		.appear = window_appear,
 	});
 
 	custom_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_COMFORTAA_40));
@@ -243,10 +295,11 @@ static void init(void) {
 		}
 	}
 
-
 	app_message_register_inbox_received(in_received_handler);
 
 	tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+
+	accel_tap_service_subscribe(&handle_accel_tap);
 
 	window_stack_push(window, true);
 }
