d6c47865d81b7858e788906655068a05a6b7c20d
Grégoire Sage <gregoire.sage@gmail.com>
2016-07-04 23:26:51 +0200

    Disable liveconfig

--- src/js/app.js
+++ src/js/app.js
@@ -3,32 +3,8 @@ var package = require('../../package.json');
 var Clay = require('pebble-clay');
 var clayConfig = require('./config.json');
 var customClay = require('./custom-clay');
-var clay = new Clay(clayConfig, customClay, {autoHandleEvents: false});
+var clay = new Clay(clayConfig, customClay);
 
 Pebble.addEventListener('ready', function(e) {
   console.log(package.pebble.displayName + " " + package.version + " ready !");
-});
-
-Pebble.addEventListener('showConfiguration', function(e) {
-  var websocket = new WebSocket("wss://liveconfig.fletchto99.com/receive/"+Pebble.getAccountToken()+"/"+Pebble.getWatchToken());
-  websocket.onmessage = function(message) {
-    var attr = JSON.parse(message.data);
-    var config = {};
-    config[attr.id] = attr.value;
-    console.log(JSON.stringify(config));
-    Pebble.sendAppMessage(config);
-  };
-  Pebble.openURL(clay.generateUrl());
-});
-
-Pebble.addEventListener('webviewclosed', function(e) {
-  if (e && !e.response) { return; }
-
-  // Send settings to Pebble watchapp
-  Pebble.sendAppMessage(clay.getSettings(e.response), function(e) {
-    console.log('Sent config data to Pebble');
-  }, function() {
-    console.log('Failed to send config data!');
-    console.log(JSON.stringify(e));
-  });
 });
